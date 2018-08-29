dba49ce6dd4d44ac96d4b2a3f5d5853d81071b69
Victor <victor-lf@hotmail.com>
2018-08-29 05:12:36 +0000

    Add TemperatureConverter

---
+++ TemperatureConverter/app/src/main/java/com/victor/temperatureconverter/MainActivity.java
@@ -0,0 +1,67 @@
+package com.victor.temperatureconverter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private EditText text;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = findViewById(R.id.inputValue);
+    }
+
+    // this method is called at button click because we assigned the name to the
+    // "OnClick" property of the button
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                RadioButton celsiusButton = findViewById(R.id.radio0);
+                RadioButton fahrenheitButton = findViewById(R.id.radio1);
+                if (text.getText().length() == 0) {
+                    Toast.makeText(this, "Please enter a valid number",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                float inputValue = Float.parseFloat(text.getText().toString());
+                if (celsiusButton.isChecked()) {
+                    text.setText(String
+                            .valueOf(ConverterUtil.convertFahrenheitToCelsius(inputValue)));
+                    celsiusButton.setChecked(false);
+                    fahrenheitButton.setChecked(true);
+                } else {
+                    text.setText(String
+                            .valueOf(ConverterUtil.convertCelsiusToFahrenheit(inputValue)));
+                    fahrenheitButton.setChecked(false);
+                    celsiusButton.setChecked(true);
+                }
+                break;
+        }
+    }
+
+}
+
+
+
+
+// Convert from Celsius to Fahrenheit and vice versa
+
+class ConverterUtil {
+    // converts to celsius
+    public static float convertFahrenheitToCelsius(float fahrenheit) {
+        return ((fahrenheit - 32) * 5 / 9);
+    }
+
+    // converts to fahrenheit
+    public static float convertCelsiusToFahrenheit(float celsius) {
+        return ((celsius * 9) / 5) + 32;
+    }
+}
+
