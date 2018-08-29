0a9ec3caf49cf3ecaa9e1815e1914105452d8451
Victor <victor-lf@hotmail.com>
2018-08-29 08:33:18 +0000

    new file:   TemperatureConverter/app/src/main/java/com/victor/temperatureconverter/ConverterUtil.java

---
+++ TemperatureConverter/app/src/main/java/com/victor/temperatureconverter/ConverterUtil.java
@@ -0,0 +1,15 @@
+package com.victor.temperatureconverter;
+
+// Convert from Celsius to Fahrenheit and vice versa
+
+public class ConverterUtil {
+    // converts to celsius
+    public static float convertFahrenheitToCelsius(float fahrenheit) {
+        return ((fahrenheit - 32) * 5 / 9);
+    }
+
+    // converts to fahrenheit
+    public static float convertCelsiusToFahrenheit(float celsius) {
+        return ((celsius * 9) / 5) + 32;
+    }
+}
--- TemperatureConverter/app/src/main/java/com/victor/temperatureconverter/MainActivity.java
+++ TemperatureConverter/app/src/main/java/com/victor/temperatureconverter/MainActivity.java
@@ -47,16 +47,3 @@ public class MainActivity extends Activity {
     }
 
 }
-
-// Convert from Celsius to Fahrenheit and vice versa
-class ConverterUtil {
-    // converts to celsius
-    public static float convertFahrenheitToCelsius(float fahrenheit) {
-        return ((fahrenheit - 32) * 5 / 9);
-    }
-
-    // converts to fahrenheit
-    public static float convertCelsiusToFahrenheit(float celsius) {
-        return ((celsius * 9) / 5) + 32;
-    }
-}
